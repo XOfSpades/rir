1b5c638e08bcfe204922abf57c3008e4735b69d6
bstoecker <bernhard.stoecker@invision.de>
2015-09-08 22:36:12 +0200

    Read sensitive data from env vars and add app.prod.config

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,12 +4,12 @@ use Mix.Config
 # you likely want to automate and keep it away from
 # your version control system.
 config :rir, Rir.Endpoint,
-  secret_key_base: "some#key"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
 config :rir, Rir.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("DATABASE_USERNAME"),
+  password: System.get_env("DATABASE_PASSWORD"),
   database: "rir_prod",
   size: 20 # The amount of database connections in the pool
