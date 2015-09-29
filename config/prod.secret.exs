d600076e672dfd31344960210456bb2c04aa0f34
XOfSpades <stoecki@der-ball-ist-rund.net>
2015-09-29 23:53:19 +0200

    Fix db config for Heroku

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -9,7 +9,8 @@ config :rir, Rir.Endpoint,
 # Configure your database
 config :rir, Rir.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: System.get_env("DATABASE_USERNAME"),
-  password: System.get_env("DATABASE_PASSWORD"),
-  database: "rir_prod",
+  url: System.get_env("DATABASE_URL"),
+  # username: System.get_env("DATABASE_USERNAME"),
+  # password: System.get_env("DATABASE_PASSWORD"),
+  # database: "rir_prod",
   pool_size: 20 # The amount of database connections in the pool
