9effa3081eeb674b27344ed9a581e1d1e36b51b1
XOfSpades <stoecki@der-ball-ist-rund.net>
2015-10-05 08:08:20 +0200

    Improve we template styling

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule Rir.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.2"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"}
+    ]
   end
 end
