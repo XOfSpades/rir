198c198642d74859f7ab41982feee572417b34b4
XOfSpades <stoecki@der-ball-ist-rund.net>
2015-10-13 23:45:23 +0200

    Add user model + tests

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -3,7 +3,7 @@ defmodule Rir.UserTest do
 
   alias Rir.User
 
-  @valid_attrs %{crypted_password: "some content", email: "some content"}
+  @valid_attrs %{ password: "some content", email: "some@content" }
 
   test "changeset with valid attributes" do
     changeset = User.changeset(%User{}, @valid_attrs)
@@ -15,12 +15,22 @@ defmodule Rir.UserTest do
     changeset1 = User.changeset(%User{}, invalid_attrs1)
     refute changeset1.valid?
 
-    invalid_attrs2 = %{email: "some content"}
+    invalid_attrs2 = %{ email: "some@content" }
     changeset2 = User.changeset(%User{}, invalid_attrs2)
     refute changeset2.valid?
 
-    invalid_attrs3 = %{crypted_password: "some content"}
+    invalid_attrs3 = %{ password: "some content" }
     changeset3 = User.changeset(%User{}, invalid_attrs3)
     refute changeset3.valid?
+  end
+
+  test "password has at least 8 characters" do
+    invalid_attrs = %{ password: "1234567", email: "some@content" }
+    changeset1 = User.changeset(%User{}, invalid_attrs)
+    refute changeset1.valid?
+
+    valid_attrs = %{ password: "12345678", email: "some@content" }
+    changeset2 = User.changeset(%User{}, valid_attrs)
+    assert changeset2.valid?
   end
 end
--- web/models/user.ex
+++ web/models/user.ex
@@ -4,12 +4,18 @@ defmodule Rir.User do
   schema "users" do
     field :email, :string
     field :crypted_password, :string
-
+    field :password, :string, virtual: true
     timestamps
   end
 
-  @required_fields ~w(email crypted_password)
+  @required_fields ~w(email password)
   @optional_fields ~w()
+
+  def create(changeset, repo) do
+    changeset
+    # |> put_change(:crypted_password, hashed_password(changeset.params["password"]))
+    |> repo.insert()
+  end
 
   @doc """
   Creates a changeset based on the `model` and `params`.
@@ -20,5 +26,8 @@ defmodule Rir.User do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    # |> unique_constraint(:email, on: Rir.Repo, downcase: true)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 8)
   end
 end
