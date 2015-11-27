76f6e90acf315bb7908826575e4e26897a430239
XOfSpades <stoecki@der-ball-ist-rund.net>
2015-11-27 21:49:49 +0100

    Include impressum in seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -32,10 +32,65 @@ member3 = %Member{
 changeset = Rir.User.changeset(
   %Rir.User{}, %{ password: "cmgcmgcmg", email: "gosewisch@rae-cmg.de" }
 )
-Rir.User.create(changeset, Rir.Repo)
+Rir.User.create(changeset, Repo)
 
 Enum.each([member1, member2, member3], fn(item) ->
-  Rir.Repo.insert!(item) end
+  Repo.insert!(item) end
 )
+
+bar_association = %{
+    name: "  Rechtsanwaltskammer Köln ",
+    street: "Riehler Straße 30",
+    town: " 50668 Köln ",
+    phone: "0221/973010-0",
+    fax: "0221/973010-50",
+    mail: "kontakt@rak-koeln.de",
+    web: "http://www.rak-koeln.de"
+  }
+
+impressum_changeset = Rir.Impressum.changeset(
+  %Rir.Impressum{},
+  %{
+    guarantor: "  Rechtsanwalt Wolfhard Gosewisch",
+    fax: "02205/84065 ",
+    phone: "02205/83396 ",
+    street: "Hauptstr. 18",
+    town: "51503 Rösrath",
+    mail: "info@rae-cmg.de",
+    web: "www.recht-in-roesrath.de",
+    turnover_tax_nr: "204/5054/0698",
+    turnover_tax_id: "fehlt",
+    lawyer_info: "Die Rechtsanwälte Nikolaus Christ, Manfred Maleika und " <>
+                 "Wolfhard Gosewisch sind in der Bundesrepublik Deutschland " <>
+                 "als Rechtsanwälte zugelassen und gehören der " <>
+                 "Rechtsanwaltskammer Köln an. Die gesetzliche " <>
+                 "Berufsbezeichnung \"Rechtsanwalt\" wurde ihnen in der " <>
+                 "Bundesrepublik Deutschland verliehen, " <>
+                 "(§ 5 Abs. 1 Nr. 5b TMG).",
+    bar_association: bar_association,
+    liability_insurance: "Allianz Versicherungs-AG, 10900 Berlin, " <>
+                         "Telefon: +49 (0)40 6946933735",
+    additional_information: "Die auf dem Server bereitgestellten Angaben " <>
+                            "wurden sorgfältig geprüft. Dessen ungeachtet " <>
+                            "kann keine Gewähr für die Korrektheit, " <>
+                            "Vollständigkeit, Qualität oder letzte " <>
+                            "Aktualität der Angaben bzw. bereitgestellten " <>
+                            "Informationen übernommen werden.",
+    copyright: "Sämtliche auf dieser Homepage verwendeten Texte, Bilder, " <>
+               "Grafiken sowie Layout dieser Seiten unterliegen dem " <>
+               "weltweit geltenden Urheberrecht. Die unerlaubte Verwendung, " <>
+               "Reproduktion oder Weitergabe einzelner Inhalte oder " <>
+               "kompletter Seiten werden sowohl straf- als auch " <>
+               "zivilrechtlich verfolgt. So besteht allein auf " <>
+               "zivilrechtlicher Grundlage die Möglichkeit die Unterlassung " <>
+               "und Schadenersatz, die Überlassung oder die Vernichtung der " <>
+               "Vervielfältigungsvorrichtungen zu verlangen. Für den Fall " <>
+               "der Zuwiderhandlung können Unterlassungsansprüche durch die " <>
+               "zuständigen Gerichte mit Ordnungsgeldern bis zu 250.000,00 " <>
+               "Euro oder Ordnungshaft bis zu sechs Monaten geahndet werden."
+  }
+)
+
+Repo.insert(impressum_changeset)
 
 Mix.shell.info "Done"
