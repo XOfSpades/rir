d45dded2f4e678c32e9b48c92c57a014c8878ecc
XOfSpades <stoecki@der-ball-ist-rund.net>
2016-02-04 22:50:57 +0100

    Add react component for articles

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -9,42 +9,128 @@ import {Socket} from "phoenix"
 //    console.log("loaded add_news file");
 // });
 
+//##############################################################################
+//################################# AddArticle #################################
+//##############################################################################
 
-
-let AddNews = React.createClass({
-  onArticleAdded() {
-    console.log("Yay")
+let AddArticle = React.createClass({
+  propTypes: {
+    articleAdded: React.PropTypes.func.isRequired
   },
 
-  onErrorOccured() {
-    console.log("Oh no")
+  getInitialState() {
+    return { header: "", content: "" }
   },
 
   handleClick() {
-    $.ajax({
-      url: "/test",
-      type: "POST",
-      contentType: 'application/javascript',
-      dataType: 'json',
-      success: this.onArticleAdded,
-      error: this.onErrorOccured
-    })
+    this.props.articleAdded(this.state.header, this.state.content)
+  },
+
+  adaptHeader(event) {
+    console.log(this.state.header)
+    this.setState({header: event.target.value});
+  },
+
+  adaptContent(event) {
+    console.log(this.state.content)
+    this.setState({content: event.target.value});
   },
 
   render() {
     return(
       <div>
-        <input> Titel </input>
-        <input> Artikel </input>
+        <input onChange={this.adaptHeader}> Titel </input>
+        <input onChange={this.adaptContent}> Artikel </input>
         <button onClick={this.handleClick}> Speichern </button>
       </div>
-
     )
   }
 })
 
-ReactDOM.render(<AddNews />, document.getElementById("dashboard"))
+//##############################################################################
+//################################ ArticleList #################################
+//##############################################################################
 
+let ArticleList = React.createClass({
+  render() {
+    return(
+      <div>
+        <p>foo</p>
+      </div>
+    )
+  }
+})
+
+//##############################################################################
+//############################# ArticleContainer ###############################
+//##############################################################################
+
+
+let ArticleContainer = React.createClass({
+  componentDidMount() {
+    this.loadArticles()
+  },
+
+  getInitialState() {
+    return { articles: [] }
+  },
+
+
+  loadArticles() {
+    var that = this
+    $.ajax({
+      url: "/api/aktuelles",
+      type: "GET",
+      contentType: 'application/json',
+      dataType: 'json',
+      success: function(response) {
+        that.setState({articles: response.articles})
+      },
+      error: this.onErrorOccured
+    })
+  },
+
+  articleAdded(header, content) {
+    var that = this
+    console.log(header)
+    console.log(content)
+    $.ajax({
+      url: "/api/aktuelles",
+      type: "POST",
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({ header: header, content: content }),
+      success: that.loadArticles(),
+      error: console.log("An error occured")
+    })
+  },
+
+  showNewArticleForm() {
+    return <AddArticle articleAdded={this.articleAdded} />
+  },
+
+  showArticles() {
+    return <ArticleList articles={this.state.articles} />
+  },
+
+  render() {
+    //TODO: set loading indicator
+    return(
+      <div>
+        <div className="new-articles-container">
+          {this.showNewArticleForm()}
+        </div>
+        <div className="articles-container">
+          {this.showArticles()}
+        </div>
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(
+  <ArticleContainer />, document.getElementById("articles-container")
+)
 
 let App = {
 
